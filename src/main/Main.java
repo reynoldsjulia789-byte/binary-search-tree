18d192eb36b781bc08621f18146fc466766fc5c8
reynoldsjulia789-byte <jreynolds21@ewu.edu>
2026-03-09 10:54:09 -0700

    update

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,10 +6,14 @@ public class Main
     {
         BinarySearchTree<Integer> intTree;
 
+        System.out.println("Binary Search Tree Demo\r\n");
+
+        System.out.println("numbers inserted:\t5 3 9 1 4 6 2\r\n");
+
         intTree = new BinarySearchTree<>(5, 3, 9, 1, 4, 6, 2);
 
-        System.out.println("preorder:   " + intTree.preorderToString());
-        System.out.println("inorder:    " + intTree.inorderToString());
-        System.out.println("postorder:  " + intTree.postorderToString());
+        System.out.println("preorder:\t" + intTree.preorderToString());
+        System.out.println("inorder:\t" + intTree.inorderToString());
+        System.out.println("postorder:\t" + intTree.postorderToString());
     }
 }
