4e2154a55a8481c9aef8fb2ff4276e2132d51933
reynoldsjulia789-byte <jreynolds21@ewu.edu>
2026-03-10 09:30:04 -0700

    updated tests for lookup method

--- src/test/BinarySearchTreeTest.java
+++ src/test/BinarySearchTreeTest.java
@@ -2,6 +2,7 @@ package test;
 
 import main.BinarySearchTree;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,37 +20,69 @@ public class BinarySearchTreeTest
         assertEquals("1 2 3", test.inorderToString());
     }
 
-    @Test
-    @DisplayName("sorts ints correctly")
-    public void sortsInts()
+    @Nested
+    @DisplayName("sort tests")
+    class sortTests
     {
-        BinarySearchTree<Integer> test;
+        @Test
+        @DisplayName("sorts ints correctly")
+        public void sortsInts()
+        {
+            BinarySearchTree<Integer> test;
 
-        test = new BinarySearchTree<>(3, 5, 2, 1, 4);
+            test = new BinarySearchTree<>(3, 5, 2, 1, 4);
 
-        assertEquals("1 2 3 4 5", test.inorderToString());
+            assertEquals("1 2 3 4 5", test.inorderToString());
+        }
+
+        @Test
+        @DisplayName("sorts Strings correctly")
+        public void sortsStrings()
+        {
+            BinarySearchTree<String> test;
+
+            test = new BinarySearchTree<>("orange", "apple", "pear", "banana", "raspberry");
+
+            assertEquals("apple banana orange pear raspberry", test.inorderToString());
+        }
     }
 
-    @Test
-    @DisplayName("sorts Strings correctly")
-    public void sortsStrings()
+    @Nested
+    @DisplayName("lookup tests")
+    class lookupTests
     {
-        BinarySearchTree<String> test;
+        @Test
+        @DisplayName("lookup returns level")
+        public void lookupTest()
+        {
+            BinarySearchTree<Integer> test;
 
-        test = new BinarySearchTree<>("orange", "apple", "pear", "banana", "raspberry");
+            test = new BinarySearchTree<>(3, 5, 2, 1, 4);
 
-        assertEquals("apple banana orange pear raspberry", test.inorderToString());
-    }
+            assertEquals(3, test.lookup(1));
+        }
 
-    @Test
-    @DisplayName("lookup returns level")
-    public void lookupTest()
-    {
-        BinarySearchTree<Integer> test;
+        @Test
+        @DisplayName("lookup returns -1 if no data found")
+        public void lookupNotFound()
+        {
+            BinarySearchTree<Integer> test;
 
-        test = new BinarySearchTree<>(3, 5, 2, 1, 4);
+            test = new BinarySearchTree<>(3, 5, 2, 1, 4);
 
-        assertEquals(3, test.lookup(1));
+            assertEquals(-1, test.lookup(0));
+        }
+
+        @Test
+        @DisplayName("lookup finds data in node at the top of the tree")
+        public void lookupTopOfTree()
+        {
+            BinarySearchTree<Integer> test;
+
+            test = new BinarySearchTree<>(3, 5, 2, 1, 4);
+
+            assertEquals(1, test.lookup(3));
+        }
     }
 
 }
