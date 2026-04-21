5691c2f6921582c5a502f9d2d3660d6e9eb91650
reynoldsjulia789 <jreynolds21@ewu.edu>
2026-04-21 11:33:40 -0700

    height method tests

--- src/test/BinarySearchTreeTest.java
+++ src/test/BinarySearchTreeTest.java
@@ -57,4 +57,40 @@ public class BinarySearchTreeTest
             assertEquals("apple, banana, orange, pear, raspberry", test.inOrder());
         }
     }
+
+    @Nested
+    @DisplayName("tree height tests")
+    class heightTests
+    {
+        @Test
+        @DisplayName("returns correct height of tree")
+        public void height()
+        {
+            BinarySearchTree<Integer> test;
+
+            test = new BinarySearchTree<>(5, 1, 3, 2, 0, 8, 6, 9, 7);
+
+            assertEquals(4, test.height());
+        }
+
+        @Test
+        @DisplayName("returns height of 0 for empty tree")
+        public void heightEmptyTree()
+        {
+            BinarySearchTree<Integer> test = new BinarySearchTree<>();
+
+            assertEquals(0, test.height());
+        }
+
+        @Test
+        @DisplayName("returns height of 1 for tree with root only")
+        public void heightRootOnlyTree()
+        {
+            BinarySearchTree<Integer> test;
+
+            test = new BinarySearchTree<>(1);
+
+            assertEquals(1, test.height());
+        }
+    }
 }
